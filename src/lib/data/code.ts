a85cf63d56565d09665b229be09e31b45ce2fefe
DebordCompany <debord.info@gmail.com>
2024-08-23 18:56:39 +0200

    refactor(menu and codes): externalize code to files

---
+++ src/lib/data/code.ts
@@ -0,0 +1,16 @@
+export const codeExampleTitle = `<Title level="p" variant="h1" className="text-white">
+    Etiqueta "p" con apariencia de h1
+</Title>
+<Title level="h1" variant="h2" className="text-amber-100">
+    Etiqueta "h1" con apariencia de h3
+</Title>
+<Title level="h4" variant="h3" className="text-amber-300">
+    Etiqueta "h4" con apariencia de h3
+</Title>
+<Title level="h3" variant="h4" className="text-amber-500">
+    Etiqueta "h3" con apariencia de h4
+</Title>
+<Title level="h6" variant="h5" className="text-amber-700">
+    Etiqueta "h6" con apariencia de h5
+</Title>
+`;
---
+++ src/lib/data/componetsHeader.ts
@@ -0,0 +1,14 @@
+export const components = [
+    {
+      id: 1,
+      route: "/design/button",
+      name: "Button",
+      description: "Get a button component",
+    },
+    {
+      id: 2,
+      route: "/design/title",
+      name: "Title",
+      description: "Get a Title component",
+    },
+  ];
