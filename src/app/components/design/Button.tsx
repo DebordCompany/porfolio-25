f57c74a7f837171da20b45a4e7ed87a7f3ce6aff
DebordCompany <debord.info@gmail.com>
2024-08-23 18:55:50 +0200

    feat(button component): change button styles

--- src/app/components/design/Button.tsx
+++ src/app/components/design/Button.tsx
@@ -1,31 +1,50 @@
 "use client";
-import { ButtonHTMLAttributes, FC } from "react";
+import { ButtonHTMLAttributes, FC, ReactNode } from "react";
 import cn from "@/lib/utils/components";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Span } from "next/dist/trace";
 
-const buttonVariants = cva("px-6 py-2 uppercase shadow-xl transition-all", {
-  variants: {
-    colors: {
-      default: "bg-yellow-500",
-      primary: "bg-neutral-400",
+const buttonVariants = cva(
+  "px-6 py-1 uppercase transition-all flex gap-2 items-center hover:scale-[1.03] text-[14px]",
+  {
+    variants: {
+      colors: {
+        default:
+          "bg-neutral-800 text-neutral-800 dark:bg-neutral-50 dark:text-neutral-50",
+        white: "bg-neutral-50 text-neutral-800",
+
+        primary: "bg-pink-500  text-pink-500",
+        secondary: "bg-blue-500  text-blue-500",
+        success: "bg-green-500  text-green-500",
+        error: "bg-red-500  text-red-500",
+        warning: "bg-amber-500  text-amber-500",
+      },
+      variant: {
+        line: "border-current border-2  bg-transparent dark:bg-transparent",
+        basic: "rounded-none border-none bg-transparent dark:bg-transparent",
+        fill: "text-neutral-50 dark:text-neutral-50",
+        vintage:
+          "border-2 border-current hover:shadow-none bg-transparent dark:bg-transparent shadow-vintage",
+      },
+      rounded: {
+        small: "rounded",
+        medium: "rounded-md",
+        big: "rounded-lg",
+        extra: "rounded-xl",
+        pill: "rounded-full",
+      },
     },
-    variant: {
-      outline:
-        "border border-yellow-500 text-yellow-500 hover:bg-yellow-400 hover:shadow-none hover:text-neutral-800  bg-transparent shadow-vintage",
-      basic: "rounded-none",
-      rounded: "rounded-lg",
-      pill: "rounded-full",
+    defaultVariants: {
+      colors: "default",
+      variant: "basic",
     },
-  },
-  defaultVariants: {
-    colors: "default",
-    variant: "basic",
-  },
-});
+  }
+);
 interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  click?: (item) => void;
+  icon?: ReactNode;
+  link?: string;
 }
 
 const Button: FC<ButtonProps> = ({
@@ -33,19 +52,18 @@ const Button: FC<ButtonProps> = ({
   className,
   colors,
   variant,
-  click,
+  rounded,
+  icon,
+  link,
   ...props
 }) => {
-  function handlerClick(item) {
-    click(item);
-  }
   return (
     <button
       {...props}
-      onClick={(item) => handlerClick(item.target)}
-      className={cn(buttonVariants({ className, colors, variant }))}
+      className={cn(buttonVariants({ className, variant, colors, rounded }))}
     >
       {children}
+      {icon && <span>{icon}</span>}
     </button>
   );
 };
