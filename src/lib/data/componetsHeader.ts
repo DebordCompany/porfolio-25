8bfbe49d6327e4965a7f808f98a5706dfefa581c
DebordCompany <debord.info@gmail.com>
2024-08-26 14:27:40 +0200

    feat: add separator page to object and change route of title

--- src/lib/data/componetsHeader.ts
+++ src/lib/data/componetsHeader.ts
@@ -1,14 +1,20 @@
 export const components = [
-    {
-      id: 1,
-      route: "/design/button",
-      name: "Button",
-      description: "Get a button component",
-    },
-    {
-      id: 2,
-      route: "/design/title",
-      name: "Title",
-      description: "Get a Title component",
-    },
-  ];
+  {
+    id: 1,
+    route: "/design/button",
+    name: "Button",
+    description: "Get a button component",
+  },
+  {
+    id: 2,
+    route: "/design/title-component",
+    name: "Title",
+    description: "Get a Title component",
+  },
+  {
+    id: 3,
+    route: "/design/separator-component",
+    name: "Separator",
+    description: "Get a Separator component",
+  },
+];
